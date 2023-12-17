ada47d3137b61cb9d4e695b8ccc6df7124f760a3
Nicolas Schneider <nicolas.schneider@fh-bielefeld.de>
2023-12-17 16:45:26 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,12 +1,12 @@
 % Initialize parameters
 rng("default");
-inp.routeLen = 100;
-ns = 75;
+inp.routeLen = 200;
+ns = 80;
 nb = 55;
 ne = 15;
 nrb = 25;
 nre = 25;
-iterations = 3000;
+iterations = 1000;
 tsp = BeeTSP(inp, ns, nb, ne, nrb, nre, iterations);
 
 tsp.solve();
