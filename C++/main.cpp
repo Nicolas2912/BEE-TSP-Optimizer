5449294cdcd1bb50767737b7ba53a1a9abf5963b
Nicolas Schneider <nicolas.schneider@fh-bielefeld.de>
2024-05-03 21:19:17 +0200

    Fix input arguments

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
+#include <iomanip> // Include for std::setw and other IO manipulators
 #include <string>
 #include "TSP.h"
 #include "Bee.h"
 
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        std::cout << "Usage: " << argv[0] << " <number of cities> <number of iterations>" << std::endl;
-        return 1;
+    int ns = 75, nb = 55, ne = 15, nrb = 25, nre = 25, routeLen = 50, iterations = 1000;
+    
+    if (argc >= 8) {
+        try {
+            ns = std::stoi(argv[1]);
+            nb = std::stoi(argv[2]);
+            ne = std::stoi(argv[3]);
+            nrb = std::stoi(argv[4]);
+            nre = std::stoi(argv[5]);
+            routeLen = std::stoi(argv[6]);
+            iterations = std::stoi(argv[7]);
+        } catch (const std::invalid_argument& ia) {
+            std::cerr << "Invalid argument: " << ia.what() << std::endl;
+            return 1;
+        } catch (const std::out_of_range& oor) {
+            std::cerr << "Argument out of range: " << oor.what() << std::endl;
+            return 1;
+        }
+    } else {
+        std::cout << "No or insufficient input arguments. Using default values." << std::endl;
     }
-    int routeLen = std::stoi(argv[1]);
-    int iterations = std::stoi(argv[2]);
-/*    int routeLen = 100;
-    int iterations = 1000;*/
 
+    std::cout << std::left; // Align text to the left
     std::cout << "Initialize Parameters" << "\n";
-    std::cout << "Number of cities: " << routeLen << "\n";
-    std::cout << "Number of iterations: " << iterations << "\n";
+    std::cout << std::setw(30) << "Number of scouts: " << std::setw(10) << ns << "\n";
+    std::cout << std::setw(30) << "Number of best sites: " << std::setw(10) << nb << "\n";
+    std::cout << std::setw(30) << "Number of elite sites: " << std::setw(10) << ne << "\n";
+    std::cout << std::setw(30) << "Number of recruited bees: " << std::setw(10) << nrb << "\n";
+    std::cout << std::setw(30) << "Number of recruited elite bees: " << std::setw(10) << nre << "\n";
+    std::cout << std::setw(30) << "Number of cities: " << std::setw(10) << routeLen << "\n";
+    std::cout << std::setw(30) << "Number of iterations: " << std::setw(10) << iterations << "\n";
+
 
     std::cout << "----------------------" << "\n";
 
     TSP tsp(routeLen);  // Create a TSP object
 
-    std::cout << "Coordinates: \n";
-    for (auto &coord: tsp.coords) {
-        std::cout << coord[0] << " " << coord[1] << "\n";
-    }
-    std::cout << "----------------------" << "\n";
-
-    Bee bee(75, 55, 15, 25, 25, iterations, tsp);  // Pass the TSP object to the Bee constructor
+    Bee bee(ns, nb, ne, nrb, nre, iterations, tsp);  // Pass the TSP object to the Bee constructor
 
     bee.solve();
 
